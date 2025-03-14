8003e985ce9e7c810ed262cd951b758b6405be92
Shamar <Shamar098@gmail.com>
2025-03-14 23:32:15 +0000

    fix: schema errors

--- documentation/Database/ItsShowtimeSchema.sql
+++ documentation/Database/ItsShowtimeSchema.sql
@@ -17,7 +17,7 @@ CREATE TABLE Cinema (
   address VARCHAR(255) NOT NULL,
   postcode VARCHAR(45) NOT NULL,
   PRIMARY KEY(`id`),
-  UNIQUE KEY `cinemaId` (`cinemaId`)
+  UNIQUE KEY (`cinemaId`)
 );
 
 -- Table structure for UserCinema
@@ -25,7 +25,7 @@ CREATE TABLE UserCinema (
   userID INT NOT NULL,
   cinemaId INT NOT NULL,
   PRIMARY KEY (userID, cinemaId),
-  KEY `cinemaId` (`cinemaId`)
+  KEY `cinemaId` (`cinemaId`),
   CONSTRAINT `usercinema_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE,
   CONSTRAINT `usercinema_ibfk_2` FOREIGN KEY (`cinemaId`) REFERENCES `cinema` (`cinemaId`) ON DELETE CASCADE
 )
