f653be5aadb3cea632af82c5d4ab5a21c882ece9
Shamar <Shamar098@gmail.com>
2025-03-26 00:40:51 +0000

    feat: add endpoint/message/button to save user cinema

--- src/main/java/com/company/showtime/controller/UserController.java
+++ src/main/java/com/company/showtime/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.company.showtime.controller;
 
+import com.company.showtime.exceptions.CustomException;
+import com.company.showtime.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *       *******Currently under construction*******
@@ -10,6 +16,21 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class UserController {
+
+    @Autowired
+    UserService service;
+
+    @PostMapping("/saveCinema")
+    public String saveUserCinema(HttpServletRequest request, RedirectAttributes redirectAttributes) throws CustomException {
+        try {
+            service.saveUserCinema(request.getParameter("cinemaId"));
+            redirectAttributes.addFlashAttribute("message", "Cinema saved successfully");
+        }catch (CustomException e){
+            redirectAttributes.addFlashAttribute("error", "Error saving cinema");
+        }
+
+        return "redirect:/cinemas";
+    }
 
 
 }
